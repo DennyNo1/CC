e39c921efc1a84e366621760789f097f643a673a
Denny <21461295@life.hkbu.edu.hk>
2022-03-13 20:11:23 +0800

    a safer version

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,7 @@ from unicodedata import name
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+import os
 import configparser
 import logging
 import redis
@@ -10,15 +11,17 @@ import redis
 global redis1
 
 def main():
-    # Load your token and create an Updater for your Bot
+   
     
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+   # Load your token and create an Updater for your Bot
+    
+    # config = configparser.ConfigParser()
+    # config.read('config.ini')
+    updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
 
     global redis1
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+    redis1 = redis.Redis(host=(os.environ['HOST']), password=(os.environ['PASSWORD']), port=(os.environ['REDISPORT']))
 
     # You can set this logging module, so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -31,7 +34,6 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
-
     dispatcher.add_handler(CommandHandler("hello", helloKevin))
 
 
